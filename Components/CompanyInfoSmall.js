d27ee978d9745dc3ffa97c170d08de728a325b35
Matthew Nordstrand <matt_nordstrand@live.com>
2021-09-07 17:02:40 -0400

    Added some formatting to the search page

--- Components/CompanyInfoSmall.js
+++ Components/CompanyInfoSmall.js
@@ -10,10 +10,17 @@ class CompanyInfoSmall extends Component {
         const { navigation } = this.props;
 
         return (
-            <View style={styles.container} onStartShouldSetResponder={() => navigation.navigate("View Stock", {symbol: this.props.stock.symbol, stockName: this.props.stock.name})}>
-                <Text>Info</Text>
-                <Text>{this.props.stock.symbol}</Text>
-                <Text>{this.props.stock.name}</Text>
+            <View
+                style={styles.container}
+                onStartShouldSetResponder={() => navigation.navigate("View Stock", {symbol: this.props.stock.symbol, stockName: this.props.stock.name})}
+            >
+                <View style={styles.nameContainer}>
+                    <Text style={styles.symbolText}>{this.props.stock.symbol}</Text>
+                    <Text style={styles.nameText}>{this.props.stock.name}</Text>
+                </View>
+                <View style={styles.statsContainer}>
+                    <Text>Hi</Text>
+                </View>
             </View>
         );
     }
@@ -23,7 +30,26 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: "#FF0000",
         margin: 5,
-    }
+        padding: 5,
+        flexDirection: "row",
+    },
+    symbolText: {
+        fontSize: 24,
+        fontWeight: "bold"
+    },
+    nameText: {
+        fontSize: 12,
+    },
+    nameContainer: {
+        flex: 2,
+        backgroundColor: "#00FF00",
+    },
+    statsContainer: {
+        flex: 1,
+        backgroundColor: "#0000FF",
+        justifyContent: "center",
+        alignItems: "flex-end",
+    },
 });
 
 export default CompanyInfoSmall;
