23dc1f60ab1722a06b4ef5b5b2583da7edfaa0ad
Matthew Nordstrand <matt_nordstrand@live.com>
2021-09-08 18:32:40 -0400

    Made more info panel on stock page

--- Components/ViewStockPage.js
+++ Components/ViewStockPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Image } from 'react-native-elements';
 import { connect } from 'react-redux';
 import Loading from './Loading';
 
@@ -13,9 +14,9 @@ function ViewStockPage(props) {
     const {route, navigation} = props;
     const { symbol } = route.params;
 
-    const portfolio = props.cache.profiles.filter(profile => profile.symbol === symbol)[0];
+    const profile = props.cache.profiles.filter(profile => profile.symbol === symbol)[0];
 
-    if (!portfolio) {
+    if (!profile) {
         return (
             <View style={styles.loadingContainer}>
                 <Loading />
@@ -25,7 +26,17 @@ function ViewStockPage(props) {
 
     return (
         <View style={styles.container}>
-            <Text>You are trying to view stock: {symbol}</Text>
+            <View style={styles.profileDescContainer}>
+                <Text style={styles.profileDescTitle}>More about {symbol}</Text>
+                <View style={styles.profileStatWrapper}>
+                    <Image style={styles.image} source={{ uri: profile.image }} />
+                    <View style={styles.statsContainer}>
+                        <Text style={styles.statDesc}>CEO <Text style={styles.statText}>{profile.ceo}</Text></Text>
+                        <Text style={styles.statDesc}>Industry <Text style={styles.statText}>{profile.industry}</Text></Text>
+                    </View>
+                </View>
+                <Text style={styles.description}>{profile.description}</Text>
+            </View>
         </View>
     );
 }
@@ -37,6 +48,47 @@ const styles = StyleSheet.create({
     },
     container: {
         padding: 5,
+    },
+    profileDescContainer: {
+        width: "95%",
+        backgroundColor: "#808080",
+        alignSelf: "center",
+        borderColor: "#000",
+        borderWidth: 2,
+        borderRadius: 20,
+    },
+    profileDescTitle: {
+        textAlign: "center",
+        fontWeight: "bold",
+        fontSize: 18,
+        backgroundColor: "#B0B0B0",
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+    },
+    profileStatWrapper: {
+        padding: 5,
+        flexDirection: "row",
+    },
+    image: {
+        width: 75,
+        height: 75,
+        borderRadius: 20,
+    },
+    statsContainer: {
+        justifyContent: "center",
+        alignItems: "flex-end",
+        flex: 1,
+    },
+    statDesc: {
+        fontWeight: "normal",
+        fontSize: 12,
+    },
+    statText: {
+        fontWeight: "bold",
+        fontSize: 18,
+    },
+    description: {
+        padding: 10,
     },
 });
 
