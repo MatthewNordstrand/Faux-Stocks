ca6487bd8f8fec9b982b307fb58637f2da310523
Matthew Nordstrand <matt_nordstrand@live.com>
2021-09-11 09:37:38 -0400

    Added stock buy to reducer

--- redux/portfolio.js
+++ redux/portfolio.js
@@ -2,6 +2,25 @@ import * as ActionTypes from './ActionTypes';
 
 export const portfolio = (state = { money: 25000, stocks: [] }, action) => {
     switch (action.type) {
+        case ActionTypes.PROFILE_STOCK_BUY:
+            const {symbol, count, price} = action.payload;
+            let ownedStock = state.stocks.filter(stock => stock.symbol === symbol)[0];
+
+            //Create new or update current owned stock reference
+            if (!ownedStock) {
+                ownedStock = {
+                    symbol: symbol,
+                    amount: count,
+                    cost: price
+                };
+            } else {
+                ownedStock.amount += count;
+                ownedStock.cost += price;
+            }
+
+            //Return state with updated stock reference and money amount
+            return {...state, money: state.money - price, stocks: [...state.stocks.filter(stock => stock.symbol !== symbol), ownedStock]};
+
         default:
             return state;
     }
