45b1380c530798f2f27358cc015b7031f95dbc22
Matthew Nordstrand <matt_nordstrand@live.com>
2021-09-11 13:09:35 -0400

    Owned shares now show up on porfolio screen with correct formatting

--- Components/ShareOwnershipInfoSmall.js
+++ Components/ShareOwnershipInfoSmall.js
@@ -33,31 +33,33 @@ class ShareOwnershipInfoSmall extends Component {
         const { navigation } = this.props;
 
         const profile = this.props.cache.profiles.filter(profile => profile.symbol === this.props.stock.symbol)[0];
+        const ownStock = this.props.portfolio.stocks.filter(stock => stock.symbol === this.props.stock.symbol)[0];
+
+        const value = (ownStock.amount * profile.price).toFixed(2);
+        const profit = ((ownStock.amount * profile.price) - ownStock.cost).toFixed(2);
 
         return (
             <TouchableOpacity
                 style={
-                    profile ? (profile.changes >= 0 ? styles.containerGreen : styles.containerRed) : styles.container
+                    profit > 0 ? styles.containerGreen : profit < 0 ? styles.containerRed : styles.container
                 }
-                onPress={() => navigation.navigate("View Stock", {symbol: this.props.stock.symbol, stockName: this.props.stock.name})}
+                onPress={() => navigation.navigate("View Stock", {symbol: profile.symbol, stockName: profile.companyName})}
             >
                 <View style={styles.nameContainer}>
                     <Text style={styles.symbolText}>{this.props.stock.symbol}</Text>
-                    <Text style={styles.nameText}>{this.props.stock.name}</Text>
+                    <Text>{profile.companyName}</Text>
+                </View>
+                <View style={styles.statsContainerOther}>
+                    <Text>{ownStock.amount}</Text>
+                    <Text>Amount</Text>
                 </View>
                 <View style={styles.statsContainer}>
-                    {profile &&
-                        <>
-                            <Icon
-                                type="ionicon"
-                                name={profile.changes >= 0 ? "caret-up" : "caret-down"}
-                                color={profile.changes >= 0 ? "#00DD00" : "#DD0000"}
-                                size={20}
-                            />
-                            <Text style={profile.changes >= 0 ? styles.priceTextUp : styles.priceTextDown}>{profile.price}</Text>
-                        </>
-                    }
-                    {!profile && <Loading showText={false} />}
+                    <Text>${value}</Text>
+                    <Text>Value</Text>
+                </View>
+                <View style={styles.statsContainerOther}>
+                    <Text>${profit}</Text>
+                    <Text>{profit >= 0 ? "Profit" : "Loss"}</Text>
                 </View>
             </TouchableOpacity>
         );
@@ -68,21 +70,21 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: "#FFFFFF",
         margin: 5,
-        padding: 5,
+        paddingHorizontal: 5,
         flexDirection: "row",
         borderRadius: 10,
     },
     containerGreen: {
         backgroundColor: "#BFFFBF",
         margin: 5,
-        padding: 5,
+        paddingHorizontal: 5,
         flexDirection: "row",
         borderRadius: 10,
     },
     containerRed: {
         backgroundColor: "#FFBFBF",
         margin: 5,
-        padding: 5,
+        paddingHorizontal: 5,
         flexDirection: "row",
         borderRadius: 10,
     },
@@ -94,13 +96,22 @@ const styles = StyleSheet.create({
         fontSize: 12,
     },
     nameContainer: {
-        flex: 2,
+        flex: 1,
     },
     statsContainer: {
         flex: 1,
-        justifyContent: "flex-end",
-        alignItems: "center",
-        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "flex-end",
+        flexDirection: "column",
+        padding: 5,
+    },
+    statsContainerOther: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "flex-end",
+        flexDirection: "column",
+        backgroundColor: "#00000050",
+        padding: 5,
     },
     priceTextUp: {
         fontSize: 18,
