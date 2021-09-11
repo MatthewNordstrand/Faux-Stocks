7f381f26539a1b3a68c52feac977c3d87f735874
Matthew Nordstrand <matt_nordstrand@live.com>
2021-09-11 09:48:10 -0400

    Fixed portfolio reducer variable names

--- redux/portfolio.js
+++ redux/portfolio.js
@@ -3,23 +3,38 @@ import * as ActionTypes from './ActionTypes';
 export const portfolio = (state = { money: 25000, stocks: [] }, action) => {
     switch (action.type) {
         case ActionTypes.PROFILE_STOCK_BUY:
-            const {symbol, count, price} = action.payload;
-            let ownedStock = state.stocks.filter(stock => stock.symbol === symbol)[0];
+            let ownedStockBuy = state.stocks.filter(stock => stock.symbol === action.payload.symbol)[0];
 
             //Create new or update current owned stock reference
-            if (!ownedStock) {
-                ownedStock = {
-                    symbol: symbol,
-                    amount: count,
-                    cost: price
+            if (!ownedStockBuy) {
+                ownedStockBuy = {
+                    symbol: action.payload.symbol,
+                    amount: action.payload.count,
+                    cost: action.payload.price
                 };
             } else {
-                ownedStock.amount += count;
-                ownedStock.cost += price;
+                ownedStockBuy.amount += action.payload.count;
+                ownedStockBuy.cost += action.payload.price;
             }
 
             //Return state with updated stock reference and money amount
-            return {...state, money: state.money - price, stocks: [...state.stocks.filter(stock => stock.symbol !== symbol), ownedStock]};
+            return {...state, money: state.money - action.payload.price, stocks: [...state.stocks.filter(stock => stock.symbol !== action.payload.symbol), ownedStockBuy]};
+
+        case ActionTypes.PROFILE_STOCK_SELL:
+            let ownedStockSell = state.stocks.filter(stock => stock.symbol === action.payload.symbol)[0];
+
+            if (ownedStockSell) {
+                ownedStockSell.amount -= action.payload.count;
+                ownedStockSell.cost -= action.payload.price;
+            }
+
+            if (ownedStockSell.amount > 0) {
+                //If we still own some stock, re-add the new stock value to state.
+                return {...state, money: state.money + action.payload.price, stocks: [...state.stocks.filter(stock => stock.symbol !== action.payload.symbol), ownedStockSell]};
+            } else {
+                //If we don't own any stock anymore, don't re-add it to state.
+                return {...state, money: state.money + action.payload.price, stocks: [...state.stocks.filter(stock => stock.symbol !== action.payload.symbol)]};
+            }
 
         default:
             return state;
