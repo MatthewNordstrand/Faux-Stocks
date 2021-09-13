17dc75388ae41b2b30992a5970d784c56324bc65
Matthew Nordstrand <matt_nordstrand@live.com>
2021-09-13 19:40:29 -0400

    Added template for api key file.

---
+++ shared/keys_example.js
@@ -0,0 +1,7 @@
+//To get this app to work with your API key, simply rename this file to "keys.js" and replace the string for the API key below with your real key.
+
+//FMP API Key:
+export const STOCK_KEY = "API_KEY_HERE";
+
+//FMP API BASE URL:
+export const STOCK_URL = "https://financialmodelingprep.com/api/v3";
