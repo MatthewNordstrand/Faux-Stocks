8569c066eda484a3b87d2acdb475f2de3ad2aedd
Matt Nordstrand <matt_nordstrand@live.com>
2021-11-14 16:37:51 -0500

    Registration support added

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const UserData = require('../models/UserData');
+const passport = require('passport');
+
 /* GET users listing. */
 router.get('/', (req, res, next) => {
     res.send('auth endpoint');
@@ -8,6 +11,34 @@ router.get('/', (req, res, next) => {
 
 router.post('/login', (req, res) => {
     res.send('login endpoint');
+});
+
+router.post('/register', (req, res) => {
+    UserData.register(
+        new UserData({ username: req.body.username }),
+        req.body.password,
+        (err, user) => {
+            if (err) {
+                res.statusCode = 500;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({success: false, status: 'Username already in use.'});
+            } else {
+                user.save(err => {
+                    if (err) {
+                        res.statusCode = 500;
+                        res.setHeader('Content-Type', 'application/json');
+                        res.json({success: false, status: 'Internal server error.'});
+                        return;
+                    }
+                    passport.authenticate('local')(req, res, () => {
+                        res.statusCode = 200;
+                        res.setHeader('Content-Type', 'application/json');
+                        res.json({success: true, status: 'Registration Successful!'});
+                    })
+                });
+            }
+        }
+    );
 });
 
 module.exports = router;
